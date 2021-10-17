d2ac692b76a11615cd2bbde8ff2d570f55d97efc
jaydip1235 <jaydipdeyalok@gmail.com>
2021-10-18 00:17:36 +0530

    Polynomial circle drawing algo added

--- main.cpp
+++ main.cpp
@@ -18,8 +18,8 @@ void DDA(){
 	int dx = x2-x1;
 	int dy = y2-y1;
 	int step =  abs(dx) > abs(dy) ? abs(dx) : abs(dy);
-	float x_inc = (float) dx / (float) step;
-    float y_inc = (float) dy / (float) step;
+	float x_inc =  dx / (float) step;
+    float y_inc =  dy / (float) step;
 	for(int i=1;i<=step;i++){
 		putpixel(round(x1),round(y1),YELLOW);
 		x1+=x_inc;
@@ -51,8 +51,40 @@ void Bresenham(){
 		 p+=(2*dy)-(2*dx);
 		 y++;
 	}
-	delay(50);
+	delay(75);
 }
+	getch();
+}
+
+void pixelSet(int xc,int yc,int x,int y)  
+{  
+    putpixel(x+xc,y+yc,LIGHTBLUE);  
+    putpixel(x+xc,-y+yc,LIGHTBLUE);  
+    putpixel(-x+xc,-y+yc,LIGHTBLUE);  
+    putpixel(-x+xc,y+yc,LIGHTBLUE);  
+    putpixel(y+xc,x+yc,LIGHTBLUE);  
+    putpixel(y+xc,-x+yc,LIGHTBLUE);  
+    putpixel(-y+xc,-x+yc,LIGHTBLUE);  
+    putpixel(-y+xc,x+yc,LIGHTBLUE);  
+}  
+
+void PolyCircle(){
+	int xc,yc,r;
+	cout<<"X-coordinate of center"<<endl;
+	cin>>xc;
+	cout<<"Y-coordinate of center"<<endl;
+	cin>>yc;
+	cout<<"Radius of Circle"<<endl;
+	cin>>r;
+	int x=0,y=r;  
+    int x2 = r/sqrt(2);  
+    while(x<=x2)  
+    {  
+        y = sqrt(r*r - x*x);  
+        pixelSet(xc,yc,x,y);  
+        delay(75);
+        x += 1;  
+    }  
 	getch();
 }
 
@@ -62,10 +94,11 @@ main()
 	cout<<"Choose an algorithm"<<endl;
 	cout<<"1.DDA"<<endl;
 	cout<<"2.Bresehnam"<<endl;
-	cout<<"3.Quit"<<endl;
+	cout<<"3.Polynomial Method(Circle)"<<endl;
+	cout<<"4.Quit"<<endl;
 	int option;
 	cin>>option;
-	while(option!=3){
+	while(option!=10){
 	switch(option){
 		case 1:
 			DDA();
@@ -73,14 +106,18 @@ main()
 		case 2:
 			Bresenham();
 			break;
+		case 3:
+			PolyCircle();
+			break;
 	}
 
 	cout<<"Choose an algorithm"<<endl;
-	cout<<"1.DDA"<<endl;
-	cout<<"2.Bresehnam"<<endl;
-	cout<<"3.Quit"<<endl;
+	cout<<"1.DDA(Line)"<<endl;
+	cout<<"2.Bresehnam(Line)"<<endl;
+	cout<<"3.Polynomial Method(Circle)"<<endl;
+	cout<<"4.Quit"<<endl;
 	cin>>option;
 			
 }
-cout<<"Thank You!";
+cout<<"Thanks";
 }
